b00a287a0df3c5ecb3542a8223bc11db785a3516
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-09-09 16:43:51 +0200

    hardcoded config urls instead of env vars

--- main.rb
+++ main.rb
@@ -6,31 +6,29 @@ require_relative "lib/etl"
 
 Dotenv.load
 
+
+
 app = nil
 OptionParser.new do |opts|
   opts.on('-a', '--app APP', Etl::VALID_APPS) { app = _1 }
 end.parse!
 
-config_url_env_var, rdv_db_url_env_var = {
-  "rdv_insertion" => [
-    "ANONYMIZER_CONFIG_RDV_INSERTION_URL",
-    "RDV_INSERTION_DB_URL"
-  ],
-  "rdv_solidarites" => [
-    "ANONYMIZER_CONFIG_RDV_SERVICE_PUBLIC_URL",
-    "RDV_SOLIDARITES_DB_URL"
-  ],
-  "rdv_service_public" => [
-    "ANONYMIZER_CONFIG_RDV_SERVICE_PUBLIC_URL",
-    "RDV_SERVICE_PUBLIC_DB_URL"
-  ]
+config_url = {
+  "rdv_insertion" => "https://raw.githubusercontent.com/adipasquale/rdv-insertion/feature/anonymizer-config/config/anonymizer.yml",
+  "rdv_solidarites" => "https://raw.githubusercontent.com/betagouv/rdv-service-public/refactor/anonymizer-gem/config/anonymizer.yml",
+  "rdv_service_public" => "https://raw.githubusercontent.com/betagouv/rdv-service-public/refactor/anonymizer-gem/config/anonymizer.yml"
+}[app]
+
+rdv_db_url_env_var = {
+  "rdv_insertion" => "RDV_INSERTION_DB_URL",
+  "rdv_solidarites" => "RDV_SOLIDARITES_DB_URL",
+  "rdv_service_public" => "RDV_SERVICE_PUBLIC_DB_URL"
 }[app]
 
 etl_db_url_env_var = "ETL_DB_URL"
 metabase_username_env_var = "METABASE_USERNAME"
 
 [
-  config_url_env_var,
   rdv_db_url_env_var,
   etl_db_url_env_var,
   metabase_username_env_var
@@ -38,7 +36,6 @@ metabase_username_env_var = "METABASE_USERNAME"
   raise "Missing environment variable #{env_var}" if ENV[env_var].blank?
 end
 
-config_url = ENV[config_url_env_var]
 rdv_db_url = ENV[rdv_db_url_env_var]
 etl_db_url = ENV[etl_db_url_env_var]
 metabase_username = ENV[metabase_username_env_var]
