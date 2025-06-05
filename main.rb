ea80f137e5fbb40649fadacff59f29b45045d90f
Victor Mours <victor@ahaoho.io>
2025-06-05 06:46:39 +0200

    Correctifs pour la compatibilité RDVSP / AEI (#11)

--- main.rb
+++ main.rb
@@ -9,16 +9,10 @@ Dotenv.load
 
 include Utils
 
-unless ENV["APP"]
-  raise "La variable d'environnement CONFIG_PATH n'est pas définie"
-end
-
-app = ENV["APP"]
-
 config_url = {
-    "rdvi" => "https://raw.githubusercontent.com/betagouv/rdv-insertion/main/config/anonymizer.yml",
-    "rdvs" => "https://raw.githubusercontent.com/betagouv/rdv-service-public/production/config/anonymizer.yml",
-    "rdvsp" => "https://raw.githubusercontent.com/betagouv/rdv-service-public/production/config/anonymizer.yml"
+  "rdvi" => "https://raw.githubusercontent.com/betagouv/rdv-insertion/main/config/anonymizer.yml",
+  "rdvs" => "https://raw.githubusercontent.com/betagouv/rdv-service-public/production/config/anonymizer.yml",
+  "rdvsp" => "https://raw.githubusercontent.com/betagouv/rdv-service-public/production/config/anonymizer.yml"
 }
 
 rdv_db_url_list = {
@@ -27,30 +21,43 @@ rdv_db_url_list = {
   "rdvsp" => "RDV_SERVICE_PUBLIC_DB_URL"
 }
 
-if config_url.key?(app)
-    config_path = config_url[app]
-    origin_db_url_env_var = rdv_db_url_list[app]
-else
-    unless ENV["CONFIG_PATH"]
-      raise "La variable d'environnement CONFIG_PATH n'est pas définie"
-    end
 
+app = ENV["APP"]
+OptionParser.new do |opts|
+  opts.on('-a', '--app APP', config_url.keys) { app = _1 }
+end.parse!
 
-    config_path = ENV["CONFIG_PATH"]
-
-    # Si le nom du fichier commence par https://, alors il s'agit d'une URL
-    if config_path.starts_with?("https://")
-      # Télécharger le fichier
-        run_command "curl -o config.yml \"#{config_path}\""
-        config_path = "config.yml"
-    end
-
-    unless File.exist?(config_path)
-      raise "La variable d'environnement CONFIG_PATH pointe vers un fichier inexistant"
-    end
-
-    origin_db_url_env_var = "ORIGIN_DB_URL"
+if app.nil?
+  raise "Définissez une variable d'environnement APP ou passez un argument --app"
 end
+
+
+
+if config_url.key?(app)
+  config_path = config_url[app]
+  origin_db_url_env_var = rdv_db_url_list[app]
+else
+  unless ENV["CONFIG_PATH"]
+    raise "La variable d'environnement CONFIG_PATH n'est pas définie"
+  end
+
+
+  config_path = ENV["CONFIG_PATH"]
+
+  origin_db_url_env_var = "ORIGIN_DB_URL"
+end
+
+# Si le nom du fichier commence par https://, alors il s'agit d'une URL
+if config_path.starts_with?("https://")
+  # Télécharger le fichier
+  run_command "curl -o config.yml \"#{config_path}\""
+  config_path = "config.yml"
+end
+
+unless File.exist?(config_path)
+  raise "La variable d'environnement CONFIG_PATH pointe vers un fichier inexistant"
+end
+
 
 etl_db_url_env_var = "ETL_DB_URL"
 metabase_username_env_var = "METABASE_USERNAME"
